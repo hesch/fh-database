e6f18ebd485a7e0981b1cc1d06739df1c59da445
Henri Schmidt <henri@reschmi.de>
2017-01-02 19:20:30 +0100

    add entity for districts

---
+++ src/main/java/de/randomerror/fh/database/entities/District.java
@@ -0,0 +1,12 @@
+package de.randomerror.fh.database.entities;
+
+import lombok.Data;
+
+/**
+ * Created by Henri on 02.01.17.
+ */
+@Data
+public class District {
+    private int id;
+    private String plz;
+}
