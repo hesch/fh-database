437feadf70eb0d95fe19bd22a87cd412490b8da7
Henri Schmidt <henri@reschmi.de>
2017-01-07 15:15:16 +0100

    check if getraenkemarkt is in lieferbezirk on change_district

--- src/main/sql/change_district.sql
+++ src/main/sql/change_district.sql
@@ -3,7 +3,7 @@ CREATE PROCEDURE `venenumBonus`.`changeDistrictOfProvider`
   (IN providerId INT, IN district INT)
 BEGIN
   DECLARE c INT;
-  SELECT COUNT(1) INTO c FROM (SELECT plz FROM lieferbezirk WHERE idLieferbezirk = district) a;
+  SELECT COUNT(1) INTO c FROM (SELECT idGetraenkemarkt FROM lieferbezirk JOIN getraenkemarkt ON lieferbezirk.plz = getraenkemarkt.plz WHERE idLieferbezirk = district) a;
   IF (c = 0) THEN
     SIGNAL SQLSTATE NULL;
   END IF;
