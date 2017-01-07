2ae2b8d08632dc3937a117dbb0959bc2c1ba5597
Henri Schmidt <henri@reschmi.de>
2017-01-07 15:26:30 +0100

    getraenkemarkt is now associated with lieferer on change_district

--- src/main/sql/change_district.sql
+++ src/main/sql/change_district.sql
@@ -2,12 +2,21 @@
 CREATE PROCEDURE `venenumBonus`.`changeDistrictOfProvider`
   (IN providerId INT, IN district INT)
 BEGIN
+  DECLARE postalCode CHAR(5);
+  SELECT plz INTO postalCode FROM lieferbezirk WHERE idLieferbezirk = district;
+
   DECLARE c INT;
-  SELECT COUNT(1) INTO c FROM (SELECT idGetraenkemarkt FROM lieferbezirk JOIN getraenkemarkt ON lieferbezirk.plz = getraenkemarkt.plz WHERE idLieferbezirk = district) a;
+  SELECT COUNT(1) INTO c FROM getraenkemarkt WHERE plz = postalCode;
   IF (c = 0) THEN
     SIGNAL SQLSTATE NULL;
   END IF;
+
   UPDATE Lieferer_lieferbezirk
   SET Lieferbezirk_idLieferbezirk = district
   WHERE Lieferer_idLieferer = providerId;
+
+  DELETE FROM getraenkemarkt_has_lieferer WHERE Lieferer_idLieferer = providerId;
+
+  INSERT INTO getraenkemarkt_has_lieferer(Lieferer_idLieferer, Getraenkemarkt_idGetraenkemarkt)
+  SELECT providerId, idGetraenkemarkt FROM getraenkemarkt WHERE plz = postalCode;
 END;
